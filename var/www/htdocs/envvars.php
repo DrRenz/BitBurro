29c2409df1ee4e30ac7888b42825ac35d6f335a1
drrenz <DrRenz@users.noreply.github.com>
2008-12-07 20:40:39 +0000

    Initial upload

---
+++ var/www/htdocs/envvars.php
@@ -0,0 +1,47 @@
+<?php
+
+#
+# SECTION SERVER SETTINGS
+#
+
+# This server's base URL (only host with method, or any subdirectory thereof)
+#$baseserver = "http://www.example.org/bitburro";
+$baseserver = "http://bitburro.example.org";
+
+# The "document root" path relative to the file system root
+$webrootroot = "/var/www/htdocs/";
+
+# The subdir path (relative to webroot) where the files will be stored 
+$filepath = "filebase";
+
+# Do we allow tickets without a "sender" email address?
+#
+# ...for creating a download-only ticket:
+$allow_anonymous_submits = "1";
+# ...for creating an upload ticket:
+$allow_anonymous_uploads = "1";
+
+
+#
+# SECTION APPEARANCE
+#
+
+# The site name, referenced on nearly every occasion :-)
+$sitename = "BitBurro";
+
+
+#
+# SECTION MAIL CONTENT
+#
+
+# Who can be contacted if everything fails
+$mailsupport = "bitburro-support@example.org";
+
+# For anonymous submits, this is the sender address in outgoing
+# emails
+#$mailfrom = "BitBurro-Server <bitburro-support@example.org>";
+$mailfrom = "BitBurro-Server <noreply@example.org>";
+
+
+# END OF SITE CONFIG
+?>
